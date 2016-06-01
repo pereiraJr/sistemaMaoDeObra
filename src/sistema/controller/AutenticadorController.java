e5aa303d42b20967cbbc38b619894cd2bde4e927
Jun!ow <Jun!ow@Lielson-PC>
2016-06-01 01:26:27 -0300

    alteração autenticador

--- src/sistema/controller/AutenticadorController.java
+++ src/sistema/controller/AutenticadorController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import sistema.dao.UsuarioDAOImplementacao;
 import sistema.model.Usuario;
 
-
+@WebServlet("/Autenticador")
 public class AutenticadorController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
